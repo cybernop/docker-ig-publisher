765a82080604ef55e20fb3a91b0f78dc546517b9
Alexander Schulze <alexander.schulze@bih-charite.de>
2023-03-02 18:40:35 +0100

    fixed paths again

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ java -jar plantuml.jar -tsvg -o $1/input/images $1/input/images-source
 
 cd $1
 $igdir/_updatePublisher.sh -y
-chmod a+x $igdir/_genonce.sh
-$igdir/_genonce.sh
+chmod a+x ./_genonce.sh
+./_genonce.sh
 cd $curr
